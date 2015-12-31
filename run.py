a059329955f07ebef48ad00a80ebd0abd0fc6f6a
das <dasarathi@gmail.com>
2015-12-30 20:57:34 -0800

    .

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    from_number = request.values.get('From', None)
- 
+    number = request.values.get('From', None)
+    message = request.values.get('Body', None)
+    path = "/conversations/" + str(number)
+    firebase = firebase.FirebaseApplication('https://burning-torch-4695.firebaseio.com', None)
+    result = firebase.post(path, number, {'msg': message})
+    print result
     resp = twilio.twiml.Response()
-    # Greet the caller by name
-    resp.message("Hello Otha")
+    resp.message(result)
  
     return str(resp)
   
