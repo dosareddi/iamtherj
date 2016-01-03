0a9b41f19eeb96729eafd8aecdc9ea84e8bd9750
das <dasarathi@gmail.com>
2016-01-02 20:03:00 -0800

    .

--- run.py
+++ run.py
@@ -86,16 +86,16 @@ slackbot.set_handler(process_slack)
 slackbot.filter_outgoing(filter_slack)
   
 sc = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16")
-if sc.rtm_connect():
-   while True:
-       messages = sc.rtm_read()
-       for m in messages:
-           if m["type"] == "message":
-               client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-               message = client.messages.create(to="+" + "12134469422", from_="+12139153611",
-                                                body=m["text"])
+#if sc.rtm_connect():
+#   while True:
+#       messages = sc.rtm_read()
+#       for m in messages:
+#           if m["type"] == "message":
+#               client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+#               message = client.messages.create(to="+" + "12134469422", from_="+12139153611",
+#                                                body=m["text"])
                
-       time.sleep(0.5)
+#       time.sleep(0.5)
 
 if __name__ == "__main__":
     app.run(debug=True)
