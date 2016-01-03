a135657650b50e381e620e48f4fcf6f644cecf53
das <dasarathi@gmail.com>
2016-01-02 19:58:44 -0800

    .

--- run.py
+++ run.py
@@ -88,7 +88,13 @@ slackbot.filter_outgoing(filter_slack)
 sc = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16")
 if sc.rtm_connect():
    while True:
-       print sc.rtm_read()
+       messages = sc.rtm_read()
+       for m in messages:
+           if m["type"] == "message":
+               client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+               message = client.messages.create(to="+" + "12134469422", from_="+12139153611",
+                                                body=m["text"])
+               
        time.sleep(0.5)
 
 if __name__ == "__main__":
