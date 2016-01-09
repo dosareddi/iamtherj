f0b8740cc976a8fc1ea8595ba330168a31bc2c70
das <dasarathi@gmail.com>
2016-01-08 19:57:45 -0800

    .

--- worker.py
+++ worker.py
@@ -7,6 +7,9 @@ from twilio.rest import TwilioRestClient
 import twilio.twiml
 import time
 
+TWILIO_ACCOUNT_SID="AC3de92c17107049aece1a2378b84b3e38"
+TWILIO_AUTH_TOKEN="10f150926cea141e336ad7864b6488e6"
+
 sc = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16")
 sc.rtm_connect()
 while True:
