f416f44da167d1fea05069c06a9fde52d2a8cb49
das <dasarathi@gmail.com>
2016-01-08 20:40:42 -0800

    .

--- worker.py
+++ worker.py
@@ -14,7 +14,8 @@ TWILIO_AUTH_TOKEN="10f150926cea141e336ad7864b6488e6"
 # P0: Get phone number from channel info.
 # P2. Persist messages to DB, or at least the last timestamp
 
-def blow():
+def get_channel_number(channel_name):
+    
   print "wtf"
 
 sc = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16")
@@ -24,7 +25,6 @@ while True:
     for m in messages:   
         if m["type"] == "message" and m.get("subtype", "") != "bot_message":
             client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-            
             message = client.messages.create(to="+" + "12134469422", from_="+12139153611",
                                              body=m["text"])
             
