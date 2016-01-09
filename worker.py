a024b1e16ac17fd6c369022998cc09244c3e93d7
das <dasarathi@gmail.com>
2016-01-08 22:21:44 -0800

    .

--- worker.py
+++ worker.py
@@ -3,6 +3,7 @@ from firebase import firebase
 from slackclient import SlackClient
 from twilio.rest import TwilioRestClient
 
+import json
 import twilio.twiml
 import time
 
@@ -18,10 +19,14 @@ slack_client = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16"
 
 def get_channel_number(channel_name):
     # Check in firebase for this channel.
-    response = slack_client.api_call("channels.info", channel=slack_name)         
+    response = slack_client.api_call("channels.info", channel=channel_name)         
     # If doesn't exist, call channels API to get info for this channel 
     # and update it.
     print response
+    response_dict = json.loads(response)
+    if response_dict["ok"]:
+        return response_dict["channel"]["name"]
+    return None
 
 
 slack_client.rtm_connect()
@@ -30,8 +35,9 @@ while True:
     for m in messages:   
         if m["type"] == "message" and m.get("subtype", "") != "bot_message":
             client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-            get_channel_number(m["channel"])
-            message = client.messages.create(to="+" + "12134469422", from_="+12139153611",
-                                             body=m["text"])
+            number = get_channel_number(m["channel"])
+            if number:
+                message = client.messages.create(to="+" + number, from_="+12139153611",
+                                                 body=m["text"])
             
     time.sleep(0.5)
