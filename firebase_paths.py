fbc6b4eaa0dc4c28f091ba054770c6ad37703577
das <dasarathi@gmail.com>
2016-01-11 21:33:46 -0800

    .

--- firebase_paths.py
+++ firebase_paths.py
@@ -11,14 +11,14 @@ CHANNELS_INFO_VAL_STATE_WAITING_FOR_WORKER = 1
 CHANNELS_INFO_VAL_STATE_WAITING_FOR_CUSTOMER = 2
 
 # Channel Worker Info is of the form /channels/12134572323/worker_info
-CHANNELS_WORKERINFO_SUBDIR = "worker_info"
+CHANNELS_WORKERINFO_SUBDIR = "/worker_info"
 CHANNELS_WORKERINFO_KEY_ID = "id"
 
 # Channel Message Info is of the form /channels/12134572323/message_info
-CHANNELS_MESSAGEINFO_SUBDIR = "message_info"
+CHANNELS_MESSAGEINFO_SUBDIR = "/message_info"
 CHANNELS_MESSAGEINFO_KEY_LAST_SENT_TS = "last_sent_ts"
 
 # This is used for mapping a slack channel id to channel name.
-SLACK_ID_NAME_CHANNEL_PATH = "/slack_id/channel"
+SLACK_ID_CHANNEL_NAME_PATH = "/slack_id/channel"
 
 
--- worker.py
+++ worker.py
@@ -7,6 +7,8 @@ import json
 import twilio.twiml
 import time
 
+import firebase_paths as fb
+
 TWILIO_ACCOUNT_SID="AC3de92c17107049aece1a2378b84b3e38"
 TWILIO_AUTH_TOKEN="10f150926cea141e336ad7864b6488e6"
 
@@ -17,47 +19,54 @@ TWILIO_AUTH_TOKEN="10f150926cea141e336ad7864b6488e6"
 
 slack_client = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16")
 
-FIREBASE_CHANNEL_NUMBERS_PATH = "/channel_numbers"
-FIREBASE_CHANNEL_TIMESTAMP_PATH = "/channel_timestamp"
+
 firebase_client  = firebase.FirebaseApplication('https://burning-torch-4695.firebaseio.com', None)
 
 
-def get_channel_number(channel_name):
+def get_channel_name(slack_channel_id):
     # Check in firebase for this channel.
-    fb_result = firebase_client.get(FIREBASE_CHANNEL_NUMBERS_PATH, channel_name)
+    fb_result = firebase_client.get(fb.SLACK_ID_CHANNEL_NAME_PATH, slack_channel_id)
     if fb_result:
         return fb_result
     # If doesn't exist, call channels API to get info for this channel 
     # and update it in firebase
-    response = slack_client.api_call("channels.info", channel=channel_name)         
+    response = slack_client.api_call("channels.info", channel=slack_channel_id)         
     response_dict = json.loads(response)
     if response_dict["ok"]:
-        number = response_dict["channel"]["name"]
-        firebase_client.put(FIREBASE_CHANNEL_NUMBERS_PATH, channel_name, number, connection=None)
-        return number
+        channel_name = response_dict["channel"]["name"]
+        firebase_client.put(fb.SLACK_ID_CHANNEL_NAME_PATH, slack_channel_id, channel_name, connection=None)
+        return channel_name
     return None
 
+# TODO(dasarathi): Filter messages starting with "<"
 def is_valid_message(message_dict):
     if (message_dict["type"] == "message" and 
-        message_dict.get("subtype", "") != "bot_message" and
+        message_dict.get("subtype", "") != "bot_message"):
+        return True
+    return False
         
 slack_client.rtm_connect()
 while True:
     messages = slack_client.rtm_read()
     for m in messages:
-        if is_valid_message(m):["type"] == "message" and m.get("subtype", "") != "bot_message":
+        if is_valid_message(m):
             client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-            number = get_channel_number(m["channel"])
+            number = get_channel_name(m["channel"])
             if not number:
                 continue
             timestamp = float(m["ts"])
             chkpoint = 0
-            channel_timestamp = firebase_client.get(FIREBASE_CHANNEL_TIMESTAMP_PATH, number)
+            channel_timestamp = firebase_client.get(
+                fb.CHANNELS_PATH + "/" + number + fb.CHANNELS_MESSAGEINFO_SUBDIR, 
+                fb.CHANNELS_MESSAGEINFO_KEY_LAST_SENT_TS)
             if channel_timestamp:
                 chkpoint = channel_timestamp
             if timestamp > chkpoint :
                 message = client.messages.create(to="+" + number, from_="+12139153611",
                                                  body=m["text"])
-                firebase_client.put(FIREBASE_CHANNEL_TIMESTAMP_PATH, number, timestamp, connection=None)
+                firebase_client.put(
+                    fb.CHANNELS_PATH + "/" + number + fb.CHANNELS_MESSAGEINFO_SUBDIR, 
+                    fb.CHANNELS_MESSAGEINFO_KEY_LAST_SENT_TS, 
+                    timestamp, connection=None)
                 
     time.sleep(0.5)
