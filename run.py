3c4ee52a8cebb1ab371026d0671072b38701e8b8
das <dasarathi@gmail.com>
2016-01-17 17:52:48 -0800

    .

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ slack_client = SlackClient("xoxp-12574501523-12578409008-17628102802-e267e28b16"
 
 SLACK_REGISTER_TOKEN = "r113P15z32hFS9ym7GFwBfR1"
 SLACK_ASSIGN_CHANNEL_TOKEN = "amtyr1lSab5DTTjqSqYqQ0yE"
+SLACK_LEAVE_CHANNEL_TOKEN = "9e1tib9ChUuOwwD0oEh7yMSB"
 
 #TODO:
 # - Don't expose phone number for channel
@@ -134,10 +135,10 @@ def leave():
     print "Leave channel invoked"
     print request
     # Check token
-#    token = request.values.get("token", None)
-#    if token != SLACK_LEAVE_CHANNEL_TOKEN:
-#        print "Invalid token\n"
-#        return None
+    token = request.values.get("token", None)
+    if token != SLACK_LEAVE_CHANNEL_TOKEN:
+        print "Invalid token\n"
+        return None
 
 ###    print request
     
