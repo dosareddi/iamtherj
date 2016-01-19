acc4b2358fdf2c3ebe67deac093cb6cb665c1b84
das <dasarathi@gmail.com>
2016-01-18 17:45:12 -0800

    .

--- worker.py
+++ worker.py
@@ -45,11 +45,10 @@ def get_channel_name(slack_channel_id):
 
     return channel_name
 
-# TODO(dasarathi): Filter messages starting with "<"
 def is_valid_message(message_dict):
     if (message_dict["type"] == "message" and 
         message_dict.get("subtype", "") != "bot_message" and
-        not message_dict["text"].startswith("<"):
+        not message_dict["text"].startswith("<")):
         return True
     return False
     
